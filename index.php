1c47e847a44c9c92f02e7350c30892dc78c0966d
MyKingCrypto <dev@mykingcrypto.io>
2018-10-04 22:42:35 +0800

    Web upload
    Testing

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<title>MyKingCrypto</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3pro.css">
+
+<body>
+
+<div class="w3-container w3-card">
+  <h1>Services</h1>
+</div>
+
+<div class="w3-container">
+<hr>
+<div class="w3-cell-row">
+  <div class="w3-cell" style="width:30%">
+    <img src="img_avatar.jpg" style="width:100%">
+  </div>
+  <div class="w3-cell w3-container">
+    <h3>WebWallet</h3>
+    <p>The WebWallet Only For (MyKingCoin) MKC.</p>
+	<p>Expired on: 24-10-2018</p>
+  </div>
+</div>  
+<hr>
+<div class="w3-cell-row">
+  <div class="w3-cell" style="width:30%">
+    <img src="img_avatar.jpg" style="width:100%">
+  </div>
+  <div class="w3-cell w3-container">
+    <h3>Explorer</h3>
+    <p>Simple Explorer For (MyKingCoin) MKC.</p>
+  </div>
+</div>
+<hr>
+
+
+<div class="w3-container">
+  <h3>Powered by Anonymous</h3>
+</div>
+
+</body>
